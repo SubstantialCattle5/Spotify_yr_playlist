fb68e2c9b561e6b6f7861a62df3aee4f2b40bda0
Nilay Nath Sharan <92233871+SubstantialCattle5@users.noreply.github.com>
2022-01-03 16:06:13 +0530

    Adding the billboard scrapper

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from bs4 import BeautifulSoup
 from date import Date_Check
+import requests
 
-date = Date_Check()
+text = requests.get(
+    'https://www.billboard.com/charts/hot-100/2016-08-20/')
 
-print(date.date)
+soup = BeautifulSoup(text.text, 'html.parser')
+songs = soup.select(selector="li h3")
 
+song_titles = [song.getText().strip("\n") for song in songs[:100]]
+print(song_titles)
